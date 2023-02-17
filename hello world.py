5659e5f0ca0546bc95a856e0cd666eae81e6ef0c
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-02-17 08:26:18 -0700

    Create hello world.py

---
+++ hello world.py
@@ -0,0 +1,12 @@
+# Recommended imports for all problems
+# Some problems may require more
+import sys
+import math
+import string
+cases = int(input())
+thing = []
+for i in range(cases):
+    phrase = input()
+    thing.append(phrase)
+for i in range(len(thing)):
+    print(thing[i])

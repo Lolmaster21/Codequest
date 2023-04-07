f77ca32d2ca10a235e11d1d230904548a812c50f
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-04-07 09:27:47 -0600

    Update brick house.py

--- brick house.py
+++ brick house.py
@@ -12,12 +12,21 @@ for caseNum in range(cases):#runs a for loop for test cases
         
         #while n3 is bigger than 0, keep subtracting 5 n2 number of times and stay above 0
         #then check if what's left over is less than or EQUAL n2
-        for i in range (data[2]):
-            
-            sub = data[2] - data[1]*5
         
-        if sub > data[0]:
-            print("false")
-                    
-        else:
+        
+        sub = data[2] - data[1]*5
+        if data[2] == 0:
             print("true")
+        else:
+            if (data[1]*5) > data[2]:
+                if data[0]< data[2]:
+                    print("false")
+            else:
+                    
+                if sub > data[0]:
+                    print("false")
+                        
+                else:
+                    print("true")
+
+

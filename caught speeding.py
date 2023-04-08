5a34c31ea633ef787ffb2e5caaf3458804f718f4
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-04-08 08:30:32 -0600

    Update caught speeding.py

--- caught speeding.py
+++ caught speeding.py
@@ -1,30 +1,32 @@
 import sys
-import math
-import string
+
 
 cases = int(sys.stdin.readline().rstrip()) #reads lines and strips spaces
 
 for caseNum in range(cases):#runs a for loop for test cases
-    
-    line = sys.stdin.readline().rstrip()#reads each line
-    speed, birthday = (val for val in line.split(" "))
-    speed = int(speed)
-    if birthday == "true":
-        if speed <=65:
-            print("no ticket")
-            
-        if speed >= 65:
-            print("small ticket")
+        line = sys.stdin.readline().rstrip()
+        data = line.split(" ")#creates spaces 
+       # for i in range (len(data)):
+        #    data[i] = int(data[i])
+        data[0] = int(data[0])
         
-        if speed >= 86:
-            print("big ticket")
-    else:
-        if speed <=60:
-            print("no ticket")
-            
-        if speed >= 60:
-            print("small ticket")
+        if data[1]=="true":
+            if data[0] <=65:
+                print("no ticket")
+            elif data[0] <= 85:
+                print("small ticket")
+            else:
+                print("big ticket")
+                
+        else:
+            if data[0] <=60:
+                print("no ticket")
+            elif data[0] <= 80:
+                print("small ticket")
+            else:
+                print("big ticket")
         
-        if speed >= 81:
-            print("big ticket")
+        
+
+
 

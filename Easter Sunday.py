53ffcfff59a50803f86ddc2715857933fc87faf4
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-04-08 16:40:45 -0600

    Update Easter Sunday.py

--- Easter Sunday.py
+++ Easter Sunday.py
@@ -2,10 +2,9 @@ import sys
 import math
 import string
 
-cases = int(sys.stdin.readline().rstrip()) #reads lines and strips spaces
+cases = int(sys.stdin.readline().rstrip()) 
 
 def easter(y):
-    
     a = y% 19
 
     b = y% 4
@@ -36,13 +35,17 @@ def easter(y):
     if date  > 31:
         date -= 31
         month = "04"
+        
+        if  date == d == 28 and e == 6 and f < 19:
+            date == "18"
+            if  date == d == 29 and e == 6:
+                date == "19"
 
-
-    return(year,'/',month,'/',date)
+    print(year,'/',month,'/',date)
 
    
     
-for caseNum in range(cases):#runs a for loop for test cases
+for caseNum in range(cases):
     year = int(sys.stdin.readline().rstrip())#Gets the number of line
     print((easter(year)))
 

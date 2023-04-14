20b6b4f6470c862bea64c64cd3b9a8b3fadc7e38
Lolmaster21 <78552751+Lolmaster21@users.noreply.github.com>
2023-04-14 10:58:58 -0600

    Create Is there an echo.py

---
+++ Is there an echo.py
@@ -0,0 +1,10 @@
+import sys #needed for readline and others
+cases = int(sys.stdin.readline().rstrip())#Gets the number of line
+
+for caseNum in range(cases):
+    line = sys.stdin.readline().rstrip()#Walks through each line
+    
+    for i in range(2):
+        print(line)
+    
+
